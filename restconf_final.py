2267c8d6ff7c93494cfc257b3caa81b0a9d9bac1
Sample User <65070203@kmitl.ac.th>
2024-10-25 07:22:45 +0000

    createfunc

--- restconf_final.py
+++ restconf_final.py
@@ -3,31 +3,48 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 # Router IP Address is 10.0.15.181-184
-api_url = "<!!!REPLACEME with URL of RESTCONF Configuration API!!!>"
+api_url = "https://10.0.15.184/restconf"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
-headers = <!!!REPLACEME with Accept and Content-Type information headers!!!>
+headers = {"Accept": "application/yang-data+json",
+    "Content-Type": "application/yang-data+json"}
 basicauth = ("admin", "cisco")
 
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback65070203",
+            "description": "created loopback by RESTCONF",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [
+                    {
+                        "ip": "172.30.203.1",
+                        "netmask": "255.255.255.0"
+                    }
+                ]
+            },
+            "ietf-ip:ipv6": {}
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.put(
+        api_url + "/data/ietf-interfaces:interfaces/interface=Loopback65070203",
+        data=json.dumps(yangConfig),
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers,
         verify=False
         )
-
+    if(resp.status_code == 204):
+        return "Cannot create: Interface loopback 65070203"
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Interface loopback 65070203 is created successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
-
 
 def delete():
     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
