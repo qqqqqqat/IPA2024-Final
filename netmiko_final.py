64d222e9754221088fec53d7972579dfb2117465
Sample User <65070203@kmitl.ac.th>
2024-10-25 08:31:02 +0000

    netmiko

--- netmiko_final.py
+++ netmiko_final.py
@@ -1,12 +1,12 @@
 from netmiko import ConnectHandler
 from pprint import pprint
 
-device_ip = "<!!!REPLACEME with router IP address!!!>"
+device_ip = "10.0.15.184"
 username = "admin"
 password = "cisco"
 
 device_params = {
-    "device_type": "<!!!REPLACEME with device type for netmiko!!!>",
+    "device_type": "cisco_ios",
     "ip": device_ip,
     "username": username,
     "password": password,
@@ -19,16 +19,31 @@ def gigabit_status():
         up = 0
         down = 0
         admin_down = 0
-        result = ssh.send_command("<!!!REPLACEME with proper command!!!>", use_textfsm=True)
-        for status in result:
-            if <!!!Write code here!!!>:
-                <!!!Write code here!!!>
-                if <!!!Write code here!!!> == "up":
-                    up += 1
-                elif <!!!Write code here!!!> == "down":
-                    down += 1
-                elif <!!!Write code here!!!> == "administratively down":
-                    admin_down += 1
-        ans = <!!!Write code here!!!>
+        result = ssh.send_command("show ip interface brief", use_textfsm=True)
+        # ตรวจสอบผลลัพธ์
+        if not result:
+            print("No results returned from the command.")
+            return
+        
+        # เก็บข้อมูลสถานะของ interfaces
+        interface_status = {}
+        for interface in result:
+            int_name = interface['interface']  # เปลี่ยนจาก 'intf' เป็น 'interface'
+            int_status = interface['status']
+            interface_status[int_name] = int_status
+            
+            # นับสถานะต่าง ๆ
+            if int_status == "up":
+                up += 1
+            elif int_status == "down":
+                down += 1
+            elif int_status == "administratively down":
+                admin_down += 1
+
+        # สร้างข้อความผลลัพธ์
+        ans = ', '.join([f"{intf} {status}" for intf, status in interface_status.items()])
+        ans += f" -> {up} up, {down} down, {admin_down} administratively down"
+        
+        # แสดงผลลัพธ์
         pprint(ans)
         return ans
